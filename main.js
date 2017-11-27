e6ca537ba19eed4deb3cf0904d25207e2dca9215
pcmkv <maxwell1111@mail.ru>
2017-11-27 21:21:01 +0200

    some fix

--- main.js
+++ main.js
@@ -1,51 +1,55 @@
-var searchBtn = document.getElementById('search');
-var filmName = document.getElementById('film-name');
-var API_KEY = 'f24a0fd18f52218851075901c5a108a0';
-var listFilms;
-var listItemBlock = document.getElementById('list-item');
+const searchBtn = document.getElementById('search');
+const filmName = document.getElementById('film-name');
+const API_KEY = 'f24a0fd18f52218851075901c5a108a0';
+const listItemBlock = document.getElementById('list-item');
 searchFilm = (e) => {
     e.preventDefault();
     console.log(filmName.value);
     console.log(listItemBlock);
     listItemBlock.innerHTML = '';
-    fetch('https://api.themoviedb.org/3/search/movie?api_key=' + API_KEY + '&query=' + filmName.value)
-        .then(response => {
-                if (response.status == 200){
-                    filmName.value ='';
-                    return response.json();
+    if (filmName.value != '') {
+        fetch('https://api.themoviedb.org/3/search/movie?api_key=' + API_KEY + '&query=' + filmName.value)
+            .then(response => {
+                    if (response.status == 200) {
+                        filmName.value = '';
+                        return response.json();
+                    }
+                    throw new Error("Error fetching data");
                 }
-            throw new Error("Error fetching data");
-            }
-        ).then(data => {
-        console.log("DATA", data);
-        listFilms = data.results;
-        console.log("listFilms",listFilms.length);
+            ).then(data => {
+            console.log("DATA", data);
+            listFilms = data.results;
+            console.log("listFilms", listFilms.length);
 
-        if(data.results == 0){
-            listItemBlock.innerHTML = '<p class="MovieList__msg"> SORRY, WE DIDN\'T FIND ANYTHING (⌣́_⌣̀)</p>';
-        }else {
-            for (var i = 0; i < listFilms.length; i++) {
+            if (data.results == 0) {
+                listItemBlock.innerHTML = '<p class="MovieList__msg"> SORRY, WE DIDN\'T FIND ANYTHING (⌣́_⌣̀)</p>';
+            } else {
+                for (var i = 0; i < listFilms.length; i++) {
                     listItemBlock.innerHTML +=
                         '<div class="MovieList__item">' +
-                            '<div class="MovieCard">' +
-                                '<span class="MovieCard__rating">' + listFilms[i].vote_average + '</span>' +
-                                '<img class="MovieCard__poster"  src='+imgFullPath(listFilms[i].poster_path)+' alt="">' +
-                                '<div class="MovieInfo__info">' +
-                                    '<h2 class="MovieInfo__title">'+listFilms[i].title+'</h2>' +
-                                    '<p class="MovieInfo__descr">'+filmDescription(listFilms[i].overview)+'</p>' +
-                                    '<p class="MovieInfo__release">Release date:'+listFilms[i].release_date.split('-')[0]+'</p>' +
-                                '</div>' +
-                                '<button class="MovieCard__btn">+</button>'+
-                            '</div>' +
+                        '<div class="MovieCard">' +
+                        '<span class="MovieCard__rating">' + listFilms[i].vote_average + '</span>' +
+                        '<img class="MovieCard__poster"  src=' + imgFullPath(listFilms[i].poster_path) + ' alt="">' +
+                        '<div class="MovieInfo__info">' +
+                        '<h2 class="MovieInfo__title">' + listFilms[i].title + '</h2>' +
+                        '<p class="MovieInfo__descr">' + filmDescription(listFilms[i].overview) + '</p>' +
+                        '<p class="MovieInfo__release">Release date:' + listFilms[i].release_date.split('-')[0] + '</p>' +
+                        '</div>' +
+                        '<button class="MovieCard__btn">+</button>' +
+                        '</div>' +
                         '</div>'
+                }
             }
-        }
 
-    }).catch(error => {
-        console.error("Error: ", error);
-    });
+        }).catch(error => {
+            console.error("Error: ", error);
+        });
 
-};
+    }
+    else{
+        alert("Введите название фильма!");
+    }
+}
 
 searchBtn.addEventListener('click',searchFilm);
 
@@ -68,21 +72,3 @@ filmDescription = (filmTex) =>{
     }
     return filmTex.substr(0,100);
 };
-
-
-/*.then(function(data) {
- console.log(data);
- listFilms = data.results;
- console.log("test");
- if(data.results == 0){
- listItemBlock.innerHTML = '<p> No films</p>';
- console.log("NO FILMS!");
- }
- else {
- for (var i = 0; i < listFilms.length; i++) {
- console.log(listFilms[i].title);
- listItemBlock.innerHTML += listFilms[i].title
- }
- }
-
- }) })*/
