7ba8961d5bc65fdf2c57ba266d892434607c7e5a
pcmkv <maxwell1111@mail.ru>
2017-11-28 13:31:04 +0200

    fix

--- main.js
+++ main.js
@@ -2,43 +2,56 @@ const searchBtn = document.getElementById('search');
 const filmName = document.getElementById('film-name');
 const API_KEY = 'f24a0fd18f52218851075901c5a108a0';
 const listItemBlock = document.getElementById('list-item');
-searchFilm = (e) => {
+
+const imgFullPath = (imgEl) =>{
+
+    (imgEl == null) ? imgEl = 'https://upload.wikimedia.org/wikipedia/commons/9/9a/Нет_фото.png' : imgEl = 'https://image.tmdb.org/t/p/w300'+imgEl;
+   /* console.log(imgEl);*/
+    return imgEl;
+};
+
+const filmDescription = (filmTex) =>{
+    (filmTex == "") ? filmTex = "Описание отсутствует" : filmTex;
+    return filmTex.substr(0,100);
+};
+
+const searchFilm = (e) => {
     e.preventDefault();
     console.log(filmName.value);
     console.log(listItemBlock);
     listItemBlock.innerHTML = '';
-    if (filmName.value != '') {
+    if (filmName.value !== '') {
         fetch('https://api.themoviedb.org/3/search/movie?api_key=' + API_KEY + '&query=' + filmName.value)
             .then(response => {
                     if (response.status == 200) {
-                        filmName.value = '';
                         return response.json();
                     }
                     throw new Error("Error fetching data");
                 }
             ).then(data => {
             console.log("DATA", data);
-            listFilms = data.results;
+            const listFilms = data.results;
             console.log("listFilms", listFilms.length);
 
-            if (data.results == 0) {
+            if (listFilms.length === 0) {
                 listItemBlock.innerHTML = '<p class="MovieList__msg"> SORRY, WE DIDN\'T FIND ANYTHING (⌣́_⌣̀)</p>';
             } else {
-                for (var i = 0; i < listFilms.length; i++) {
+                listFilms.forEach (function (el) {
+                    console.log("EL",el);
                     listItemBlock.innerHTML +=
-                        '<div class="MovieList__item">' +
-                        '<div class="MovieCard">' +
-                        '<span class="MovieCard__rating">' + listFilms[i].vote_average + '</span>' +
-                        '<img class="MovieCard__poster"  src=' + imgFullPath(listFilms[i].poster_path) + ' alt="">' +
-                        '<div class="MovieInfo__info">' +
-                        '<h2 class="MovieInfo__title">' + listFilms[i].title + '</h2>' +
-                        '<p class="MovieInfo__descr">' + filmDescription(listFilms[i].overview) + '</p>' +
-                        '<p class="MovieInfo__release">Release date:' + listFilms[i].release_date.split('-')[0] + '</p>' +
-                        '</div>' +
-                        '<button class="MovieCard__btn">+</button>' +
-                        '</div>' +
-                        '</div>'
-                }
+                        `<div class="MovieList__item">
+                            <div class="MovieCard">
+                                <span class="MovieCard__rating">${el.vote_average}</span>
+                                <img class="MovieCard__poster"  src='${imgFullPath(el.poster_path)}' alt="">
+                                <div class="MovieInfo__info">
+                                    <h2 class="MovieInfo__title">${el.title}</h2>
+                                    <p class="MovieInfo__descr">${filmDescription(el.overview)}</p>
+                                    <p class="MovieInfo__release">Release date:${el.release_date.split('-')[0]}</p>
+                                </div>
+                                <button class="MovieCard__btn">+</button>
+                            </div>
+                        </div>`
+                });
             }
 
         }).catch(error => {
@@ -49,26 +62,7 @@ searchFilm = (e) => {
     else{
         alert("Введите название фильма!");
     }
-}
+    filmName.value = '';
+};
 
 searchBtn.addEventListener('click',searchFilm);
-
-imgFullPath = (imgEl) =>{
-    if(imgEl == null){
-        imgEl = "https://upload.wikimedia.org/wikipedia/commons/9/9a/Нет_фото.png"
-    }
-    else{
-        imgEl = "https://image.tmdb.org/t/p/w300/"+imgEl;
-    }
-    return imgEl;
-};
-
-filmDescription = (filmTex) =>{
-    if(filmTex == ""){
-        filmTex = "Описание отсутствует"
-    }
-    else{
-        filmTex+=filmTex;
-    }
-    return filmTex.substr(0,100);
-};
