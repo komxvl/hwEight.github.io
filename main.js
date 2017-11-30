351aaf7ca53ea99c11c0064267b6763b78e2c296
pcmkv <maxwell1111@mail.ru>
2017-11-30 09:32:05 +0200

    fix

--- main.js
+++ main.js
@@ -1,22 +1,24 @@
 const searchBtn = document.getElementById('search');
+const popularBtn = document.getElementById('popular');
+const topRated = document.getElementById('top-rated');
+const upComing = document.getElementById('up-coming');
 const filmName = document.getElementById('film-name');
 const API_KEY = 'f24a0fd18f52218851075901c5a108a0';
 const listItemBlock = document.getElementById('list-item');
 
-const imgFullPath = (imgEl) =>{
-
-    (imgEl == null) ? imgEl = 'https://upload.wikimedia.org/wikipedia/commons/9/9a/Нет_фото.png' : imgEl = 'https://image.tmdb.org/t/p/w300'+imgEl;
-   /* console.log(imgEl);*/
-    return imgEl;
+const imgFullPath = imgEl =>{
+    return (imgEl === null) ? 'https://upload.wikimedia.org/wikipedia/commons/9/9a/Нет_фото.png' : `https://image.tmdb.org/t/p/w300${imgEl}`;
 };
 
 const filmDescription = (filmTex) =>{
-    (filmTex == "") ? filmTex = "Описание отсутствует" : filmTex;
-    return filmTex.substr(0,100);
+    return (filmTex === "") ? "Описание отсутствует" : filmTex.substr(0,100);
 };
+
+
 
 const searchFilm = (e) => {
     e.preventDefault();
+    let allFilms = "";
     console.log(filmName.value);
     console.log(listItemBlock);
     listItemBlock.innerHTML = '';
@@ -38,8 +40,7 @@ const searchFilm = (e) => {
             } else {
                 listFilms.forEach (function (el) {
                     console.log("EL",el);
-                    listItemBlock.innerHTML +=
-                        `<div class="MovieList__item">
+                    allFilms += `<div class="MovieList__item">
                             <div class="MovieCard">
                                 <span class="MovieCard__rating">${el.vote_average}</span>
                                 <img class="MovieCard__poster"  src='${imgFullPath(el.poster_path)}' alt="">
@@ -50,7 +51,9 @@ const searchFilm = (e) => {
                                 </div>
                                 <button class="MovieCard__btn">+</button>
                             </div>
-                        </div>`
+                        </div>`;
+                    listItemBlock.innerHTML = allFilms;
+
                 });
             }
 
@@ -64,5 +67,47 @@ const searchFilm = (e) => {
     }
     filmName.value = '';
 };
-
 searchBtn.addEventListener('click',searchFilm);
+
+
+
+const filmRating = (param) => {
+    listItemBlock.innerHTML = '';
+    let films ="";
+    fetch(`https://api.themoviedb.org/3/movie/${param}?api_key=` + API_KEY)
+        .then(response => {
+                if (response.status == 200) {
+                    return response.json();
+                }
+                throw new Error("Error fetching data");
+            }
+        ).then(data => {
+        const listFilms = data.results;
+        console.log("listFilms", listFilms.length);
+        listFilms.forEach (function (el) {
+            console.log("EL",el);
+            films +=
+                `<div class="MovieList__item">
+                            <div class="MovieCard">
+                                <span class="MovieCard__rating">${el.vote_average}</span>
+                                <img class="MovieCard__poster"  src='${imgFullPath(el.poster_path)}' alt="">
+                                <div class="MovieInfo__info">
+                                    <h2 class="MovieInfo__title">${el.title}</h2>
+                                    <p class="MovieInfo__descr">${filmDescription(el.overview)}</p>
+                                    <p class="MovieInfo__release">Release date:${el.release_date.split('-')[0]}</p>
+                                </div>
+                                <button class="MovieCard__btn">+</button>
+                            </div>
+                        </div>`;
+            listItemBlock.innerHTML = films;
+        });
+    });
+};
+
+popularBtn.addEventListener('click',function(){filmRating("popular")});
+topRated.addEventListener('click',function(){filmRating("top_rated")});
+upComing.addEventListener('click',function(){filmRating("upcoming")});
+
+
+
+
